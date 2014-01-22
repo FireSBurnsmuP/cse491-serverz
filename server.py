9b8495b00e65d1211958591bbcae70f901f65258
Chris Thomas <firesburnsmup@gmail.com>
2014-01-22 17:17:19 -0500

    hw2 changes   - added functions   - changed EOF handling

--- server.py
+++ server.py
@@ -3,38 +3,45 @@ import random
 import socket
 import time
 
-s = socket.socket()         # Create a socket object
-host = socket.getfqdn() # Get local machine name
-port = random.randint(8000, 9999)
-s.bind((host, port))        # Bind to the port
+def main():
+  "Starts up the server and waits for connections"
 
-print 'Starting server on', host, port
-print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+  # Create a socket object
+  s = socket.socket()
+  # Get local machine name
+  host = socket.getfqdn()
+  port = random.randint(8000, 9999)
+  # Bind to the port
+  s.bind((host, port))
+  print 'Starting server on', host, port
+  print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+  # Now wait for client connection.
+  s.listen(5)
 
-s.listen(5)                 # Now wait for client connection.
-
-# This code was jbull477's, I just added the DOCTYPE.
-htmlResponse = """
-
-HTTP/1.0 200 OK
-Content-Type: text/html
-
-<!DOCTYPE html>
-<html>
-  <body>
-  <h1>Hello, world</h1> this is fires' Web server.
-  </body>
-</html>
-
-"""
-print 'Entering infinite loop; hit CTRL-C to exit'
-while True:
-    # Establish connection with client.    
+  print 'Entering infinite loop; hit CTRL-C to exit'
+  while True:
+    # Establish connection with client.
     c, (client_host, client_port) = s.accept()
     print c.recv(1000)
     print 'Got connection from', client_host, client_port
-    # Also jbull477's. Turns out I was on the right track.
-    c.send(htmlResponse)
-    c.send('Thank you for connecting ')
-    c.send("good bye.")
-    c.close()
+    handle_connection(c)
+
+def handle_connection(conn):
+  "Handles a given connection by sending the proper response"
+
+  # This code was jbull477's, I just added the DOCTYPE.
+  EOF = "\r\n"
+  htmlResponse = "HTTP/1.0 200 OK" + EOF
+  htmlResponse += "Content-Type: text/html" + EOF
+  htmlResponse += EOF
+  htmlResponse += "<!DOCTYPE html>" + EOF
+  htmlResponse += "<html>" + EOF
+  htmlResponse += "  <body>" + EOF
+  htmlResponse += "  <h1>Hello, world</h1> this is fires' Web server." + EOF
+  htmlResponse += "  </body>" + EOF
+  htmlResponse += "</html>" + EOF
+  conn.send(htmlResponse)
+  conn.close()
+
+if __name__ == '__main__':
+  main()
