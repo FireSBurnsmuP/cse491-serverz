e3333b68b9614562cd2d18655752f8d3c6b22ef8
Chris Thomas <firesburnsmup@gmail.com>
2014-01-23 00:37:48 -0500

    hw2 changes continued

--- server.py
+++ server.py
@@ -22,7 +22,6 @@ def main():
     while True:
         # Establish connection with client.
         c, (client_host, client_port) = s.accept()
-        print c.recv(1000)
         print 'Got connection from', client_host, client_port
         handle_connection(c)
 
@@ -30,17 +29,117 @@ def handle_connection(conn):
     "Handles a given connection by sending the proper response"
 
     EOF = "\r\n"
-    htmlResponse = "HTTP/1.0 200 OK" + EOF \
+    request = conn.recv(1000)
+    # parse the request line
+    request = request.splitlines()
+    request_line = request[0].split()
+    if request_line[0] == 'GET' :
+        html_response = get(request_line)
+    elif request_line[0] == 'POST':
+        html_response = post(request_line)
+    elif request_line[0] == 'HEAD':
+        html_response = head(request_line)
+    else :
+        # do other stuff
+        html_response = "HTTP/1.1 405 Method Not Allowed" + EOF + EOF
+    conn.send(html_response)
+    conn.close()
+
+def get(request_line) :
+    "Processes a get request"
+
+    EOF = "\r\n"
+    if request_line[1] == '/':
+        html_response = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF \
+                    + "<!DOCTYPE html>" + EOF \
+                    + "<html>" + EOF \
+                    + "  <body>" + EOF \
+                    + "  <h1>Hello, world</h1> this is fires' Web server." + EOF \
+                    + "  <h3>Links:</h3>" + EOF \
+                    + "  <div style=\"padding-left: 1.0em;\">" + EOF \
+                    + "    <ul>" + EOF \
+                    + "      <li><a href=\"/content\">Content</a></li>" + EOF \
+                    + "      <li><a href=\"/file\">File</a></li>" + EOF \
+                    + "      <li><a href=\"/image\">Image</a></li>" + EOF \
+                    + "    </ul>" + EOF \
+                    + "  </div>" + EOF \
+                    + "  </body>" + EOF \
+                    + "</html>" + EOF
+    elif request_line[1] == '/content':
+        html_response = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF \
+                    + "<!DOCTYPE html>" + EOF \
+                    + "<html>" + EOF \
+                    + "  <body>" + EOF \
+                    + "  <h1>Hello, world</h1> this is the content on fires' Web server." + EOF \
+                    + "  </body>" + EOF \
+                    + "</html>" + EOF
+    elif request_line[1] == '/file':
+        html_response = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF \
+                    + "<!DOCTYPE html>" + EOF \
+                    + "<html>" + EOF \
+                    + "  <body>" + EOF \
+                    + "  <h1>Hello, world</h1> this is the file on fires' Web server." + EOF \
+                    + "  </body>" + EOF \
+                    + "</html>" + EOF
+    elif request_line[1] == '/image':
+        html_response = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF \
+                    + "<!DOCTYPE html>" + EOF \
+                    + "<html>" + EOF \
+                    + "  <body>" + EOF \
+                    + "  <h1>Hello, world</h1> this is the image on fires' Web server." + EOF \
+                    + "  </body>" + EOF \
+                    + "</html>" + EOF
+    else :
+        html_response = "HTTP/1.1 404 Not Found" + EOF + EOF
+    return html_response;
+
+def post(request_line) :
+    "processes a POST request"
+
+    EOF = "\r\n"
+    html_response = "HTTP/1.1 200 OK" + EOF \
                 + "Content-Type: text/html" + EOF \
                 + EOF \
                 + "<!DOCTYPE html>" + EOF \
                 + "<html>" + EOF \
                 + "  <body>" + EOF \
-                + "  <h1>Hello, world</h1> this is fires' Web server." + EOF \
+                + "  <h1>Hello, world</h1> you've attempted to POST to fires' Web server." + EOF \
                 + "  </body>" + EOF \
                 + "</html>" + EOF
-    conn.send(htmlResponse)
-    conn.close()
+    return html_response
+
+def head(request_line):
+    "processes a HEAD request"
+
+    EOF = "\r\n"
+    if request_line[1] == '/':
+        html_response = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF
+    elif request_line[1] == '/content':
+        html_response = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF
+    elif request_line[1] == '/file':
+        html_response = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF
+    elif request_line[1] == '/image':
+        html_response = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF
+    else :
+        html_response = "HTTP/1.1 404 Not Found" + EOF + EOF
+    return html_response
+
 
 if __name__ == '__main__':
     main()
--- test_server.py
+++ test_server.py
@@ -28,17 +28,155 @@ class FakeConnection(object):
 # Test a basic GET call.
 
 def test_handle_connection():
-    conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
+    conn = FakeConnection("GET / HTTP/1.1\r\n\r\n")
     EOF = "\r\n"
-    expected_return = "HTTP/1.0 200 OK" + EOF \
-                + "Content-Type: text/html" + EOF \
-                + EOF \
-                + "<!DOCTYPE html>" + EOF \
-                + "<html>" + EOF \
-                + "  <body>" + EOF \
-                + "  <h1>Hello, world</h1> this is fires' Web server." + EOF \
-                + "  </body>" + EOF \
-                + "</html>" + EOF
+    expected_return = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF \
+                    + "<!DOCTYPE html>" + EOF \
+                    + "<html>" + EOF \
+                    + "  <body>" + EOF \
+                    + "  <h1>Hello, world</h1> this is fires' Web server." + EOF \
+                    + "  <h3>Links:</h3>" + EOF \
+                    + "  <div style=\"padding-left: 1.0em;\">" + EOF \
+                    + "    <ul>" + EOF \
+                    + "      <li><a href=\"/content\">Content</a></li>" + EOF \
+                    + "      <li><a href=\"/file\">File</a></li>" + EOF \
+                    + "      <li><a href=\"/image\">Image</a></li>" + EOF \
+                    + "    </ul>" + EOF \
+                    + "  </div>" + EOF \
+                    + "  </body>" + EOF \
+                    + "</html>" + EOF
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_get_content():
+    conn = FakeConnection("GET /content HTTP/1.1\r\n\r\n")
+    EOF = "\r\n"
+    expected_return = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF \
+                    + "<!DOCTYPE html>" + EOF \
+                    + "<html>" + EOF \
+                    + "  <body>" + EOF \
+                    + "  <h1>Hello, world</h1> this is the content on fires' Web server." + EOF \
+                    + "  </body>" + EOF \
+                    + "</html>" + EOF
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_get_image():
+    conn = FakeConnection("GET /image HTTP/1.1\r\n\r\n")
+    EOF = "\r\n"
+    expected_return = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF \
+                    + "<!DOCTYPE html>" + EOF \
+                    + "<html>" + EOF \
+                    + "  <body>" + EOF \
+                    + "  <h1>Hello, world</h1> this is the image on fires' Web server." + EOF \
+                    + "  </body>" + EOF \
+                    + "</html>" + EOF
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_get_file():
+    conn = FakeConnection("GET /file HTTP/1.1\r\n\r\n")
+    EOF = "\r\n"
+    expected_return = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF \
+                    + "<!DOCTYPE html>" + EOF \
+                    + "<html>" + EOF \
+                    + "  <body>" + EOF \
+                    + "  <h1>Hello, world</h1> this is the file on fires' Web server." + EOF \
+                    + "  </body>" + EOF \
+                    + "</html>" + EOF
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_post():
+    conn = FakeConnection("POST / HTTP/1.1\r\n\r\n")
+    EOF = "\r\n"
+    expected_return = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF \
+                    + "<!DOCTYPE html>" + EOF \
+                    + "<html>" + EOF \
+                    + "  <body>" + EOF \
+                    + "  <h1>Hello, world</h1> you've attempted to POST to fires' Web server." + EOF \
+                    + "  </body>" + EOF \
+                    + "</html>" + EOF
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_put():
+    conn = FakeConnection("PUT / HTTP/1.1\r\n\r\n")
+    EOF = "\r\n"
+    expected_return = "HTTP/1.1 405 Method Not Allowed" + EOF + EOF
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_delete():
+    conn = FakeConnection("DELETE / HTTP/1.1\r\n\r\n")
+    EOF = "\r\n"
+    expected_return = "HTTP/1.1 405 Method Not Allowed" + EOF + EOF
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_head():
+    conn = FakeConnection("HEAD / HTTP/1.1\r\n\r\n")
+    EOF = "\r\n"
+    expected_return = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_head_content():
+    conn = FakeConnection("HEAD /content HTTP/1.1\r\n\r\n")
+    EOF = "\r\n"
+    expected_return = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_head_file():
+    conn = FakeConnection("HEAD /file HTTP/1.1\r\n\r\n")
+    EOF = "\r\n"
+    expected_return = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+def test_head_image():
+    conn = FakeConnection("HEAD /image HTTP/1.1\r\n\r\n")
+    EOF = "\r\n"
+    expected_return = "HTTP/1.1 200 OK" + EOF \
+                    + "Content-Type: text/html" + EOF \
+                    + EOF
 
     server.handle_connection(conn)
 
