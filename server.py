06c929aab36a61ca37bd0fdfe048300568e7178a
Chris Thomas <firesburnsmup@gmail.com>
2014-03-20 12:22:16 -0400

    add (optional) command line options

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ Main server file.
 """
 import random
 import socket
+import argparse
 from urlparse import urlparse
 from urlparse import parse_qs
 import cgi
@@ -21,19 +22,63 @@ import imageapp
 CRLF = "\r\n"
 
 def main():
-    "Starts up the server and waits for connections"
+    """
+    Initializes the Server.
+
+    Parses command line arguments (if present),
+    then initializes the wsgi_app and starts up the server,
+    then waits for connections
+    """
+
+    parser = argparse.ArgumentParser(
+        description='A WSGI Server implemented for CSE491-001.',
+        epilog='Please check the non-existent documentation for more info.',
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    # Add the '-?' alias for '--help', which I prefer to use:
+    parser.add_argument('-?',
+        action='help',
+        help='Alias for --help')
+    # Add the application argument:
+    parser.add_argument('--app',
+        nargs='?',
+        dest='app',
+        default='fires',
+        choices=['fires', 'hw6', 'imageapp', 'quixote_demo'],
+        help='\n'.join([
+            'Which WSGI application to run.',
+            '(default: "%(default)s" - my homework 6)',
+            'Alias: -A'
+            ]))
+    parser.add_argument('-A',
+        nargs='?',
+        dest='app',
+        default='fires',
+        choices=['fires', 'hw6', 'imageapp', 'quixote_demo'],
+        help=argparse.SUPPRESS)
+    # Add the port argument:
+    parser.add_argument('--port',
+        nargs='?',
+        default=random.randint(8000, 9999),
+        type=int,
+        help='\n'.join([
+            'Which port to start the server on.',
+            '(default: random integer between 8000 and 9999)',
+            'Alias: -p'
+            ]))
+    # After that, parse the command-line arguments.
+    args = parser.parse_args()
 
     # Create a socket object
     sock = socket.socket()
     # Get local machine name
     host = socket.getfqdn()
 
-    # TODO get portnum from commandline
     if host in ('magrathea', 'Thoth'):
         # For testing, I don't want to have to change my url all the damn time.
         port = 8080
     else:
-        port = random.randint(8000, 9999)
+        port = args.port
     # Bind to the port
     # TODO figure out how to immediately unbind when I'm done
     sock.bind((host, port))
@@ -42,7 +87,7 @@ def main():
     sock.listen(5)
 
     # TODO get this from commandline
-    app_to_run = 'imageapp'
+    app_to_run = args.app
     if app_to_run == 'quixote_demo':
         # quixote stuff for testing with that
         p = create_publisher()
